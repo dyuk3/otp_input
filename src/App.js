15e1ea7ebb5220b247995d8d9f9646f5c1b2676d
unknown <vaibhavsinghyo@gmail.com>
2025-11-13 14:20:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import { useRef } from 'react';
 import './App.css';
 
 function App() {
+  const input1ref = useRef(null);
+  const input2ref = useRef(null);
+  const input3ref = useRef(null);
+  const input4ref = useRef(null);
+
+  const handleInput1Change = (e) => {
+    if (e.target.value.length === 1) {
+      input2ref.current.focus();
+    }
+  };
+
+  const handleInput2Change = (e) => {
+    if (e.target.value.length === 1) {
+      input3ref.current.focus();
+    }
+  };
+
+  const handleInput3Change = (e) => {
+    if (e.target.value.length === 1) {
+      input4ref.current.focus();
+    }
+  };
+
+  const controlInput = (ref) => {
+    if (ref.current.value.length > ref.current.maxLength)
+      ref.current.value = ref.current.value.slice(-1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <h1>Please enter your OTP:</h1>
+        <div className='inputContainer'>
+          <input
+            className='numInput'
+            ref={input1ref}
+            onChange={handleInput1Change}
+            onInput={() => controlInput(input1ref)}
+            maxLength={1}
+            type='number'
+          />
+          <input
+            className='numInput'
+            ref={input2ref}
+            onChange={handleInput2Change}
+            onInput={() => controlInput(input2ref)}
+            maxLength={1}
+            type='number'
+          />
+          <input
+            className='numInput'
+            ref={input3ref}
+            onChange={handleInput3Change}
+            onInput={() => controlInput(input3ref)}
+            maxLength={1}
+            type='number'
+          />
+          <input
+            className='numInput'
+            ref={input4ref}
+            onInput={() => controlInput(input4ref)}
+            maxLength={1}
+            type='number'
+          />
+        </div>
+        <button className='submitBtn'>Submit OTP</button>
+      </div>
     </div>
   );
 }
